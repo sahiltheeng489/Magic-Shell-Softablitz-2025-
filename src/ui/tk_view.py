f1d589c87acd0d27a645e3d85bca0990194b0299
smrutiranjan <smrutiranjanbehera444@egmail.com>
2025-10-02 19:26:34 +0530

    finally add controller in ui in place of runner

--- src/ui/tk_view.py
+++ src/ui/tk_view.py
@@ -1,6 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+print("sys.path:", sys.path)
+
 
 import tkinter as tk
 import tkinter.font as tkFont
