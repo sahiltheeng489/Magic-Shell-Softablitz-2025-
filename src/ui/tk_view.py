e6967d1e9b3d1362aba578bee4ba2e0b6b5424a5
smrutiranjan <smrutiranjanbehera444@egmail.com>
2025-10-02 19:07:31 +0530

    add controller inside ui in place of coderunner

--- src/ui/tk_view.py
+++ src/ui/tk_view.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 import tkinter.font as tkFont
-from ..core.runner import CommandRunner
+from core.controller import Controller
 
-runner = CommandRunner()
+controller = Controller()
 
 def run_command():
     user_command = entry.get()
-    output = runner.run(user_command)
+    output = controller.handle_input(user_command)
     insert_output(f"> {user_command}\n{output}\n")
     entry.delete(0, tk.END)
 
@@ -27,7 +27,7 @@ entry.pack(pady=5)
 run_button = tk.Button(window, text="Run", command=run_command)
 run_button.pack(pady=5)
 
-output_area = tk.Text(window, height=20, width=150,
+output_area = tk.Text(window, height=30, width=100,
                       bg="black", fg="lime",
                       insertbackground="white",
                       font=terminal_font,
